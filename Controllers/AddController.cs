5e7d005e887caf5eb36f42a62e4525f293b43714
Alex Klokov <klokov.alex@gmail.com>
2021-04-29 23:13:08 +0600

    Добавьте файлы проекта.

---
+++ Controllers/AddController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Add(int a, int b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+
+
+    }
+}
---
+++ Controllers/SubtractController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubtractController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Subtract(int a, int b)
+        {
+            return $"{a} - {b} = {a - b}";
+        }
+
+    }
+}
